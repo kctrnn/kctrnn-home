a161b4084b85ccd70ae3773f3d48400724ac4f49
Kim Chan <kctrnn@gmail.com>
2023-05-05 00:02:21 +0700

    add typing animation to hero title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { TypeAnimation } from 'react-type-animation';
 
+import KimChan from '@site/static/img/kctrnn.JPG';
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -12,7 +14,17 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">I'm {siteConfig.title}</h1>
+        <div className={styles.heroTitleWrapper}>
+          <img className={styles.heroTitleThumbnail} src={KimChan} alt="kctrnn thumbnail" />
+
+          <TypeAnimation
+            className="hero__title"
+            wrapper="h1"
+            sequence={['Hi, I am Kim Chan', 1000, 'Hi, I am kctrnn', 1000]}
+            repeat={Infinity}
+          />
+        </div>
+
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/blog/welcome">
